488e22814e5d5b1addefb698609f9e0892be9f86
Carlen White <whitersuburban@gmail.com>
2025-10-30 01:02:14 -0400

    Fix help casing

--- src/args.rs
+++ src/args.rs
@@ -4,15 +4,15 @@ use std::path::PathBuf;
 #[clap(author = "Carlen White", version, about)]
 /// Application configuration
 pub struct Args {
-    /// whether to be verbose
+    /// Whether to be verbose
     #[arg(long, short = 'v')]
     pub verbose: bool,
 
-    /// reshow the XDG portal configuration dialog
+    /// Reshow the XDG portal configuration dialog
     #[arg(long)]
     pub show_portal_config: bool,
 
-    /// override XDG_CONFIG_HOME/iron-button/config.yml with a different path
+    /// Override XDG_CONFIG_HOME/iron-button/config.yml with a different path
     #[arg(long)]
     pub config: Option<PathBuf>,
 }
